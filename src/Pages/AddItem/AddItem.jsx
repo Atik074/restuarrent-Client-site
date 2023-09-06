de925646475b8cab1588485f6e81c1f94ce7c91d
Atik074 <atiksmats#@gmail.com>
2023-09-07 00:16:58 +0600

    add item from added

--- src/Pages/AddItem/AddItem.jsx
+++ src/Pages/AddItem/AddItem.jsx
@@ -1,9 +1,63 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
+import { FaUtensils } from 'react-icons/fa';
 
 const AddItem = () => {
+
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const onSubmit = data => console.log(data);
+    console.log(errors);
+
+
     return (
-        <div>
-            thtis is additem 
+        <div className='w-full px-10'>
+        <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="form-control w-full ">
+          <label className="label">
+         <p className="label-text mb-1 text-[19px] font-medium">Recipe name<span className='text-red-800'>*</span></p>
+        </label>
+        <input type="name" {...register("name", {required: true, maxLength: 80})} placeholder="recepie name" className="input input-bordered w-full text-lg " />
+     </div>
+        
+  <div className='flex space-x-6 my-3'>
+      <div className="form-control w-full ">
+     <label className="label">
+    <p className="label-text mb-1 text-[19px] font-medium">Category<span className='text-red-800'>*</span> </p>
+  
+  </label>
+  <select className="select select-bordered  text-[18px]" {...register("category", { required: true })}>
+    <option elected>category</option>
+    <option>Salad</option>
+    <option>Pizza</option>
+    <option>Soup</option>
+    <option>Dessert</option>
+    <option>Drink</option>
+    </select>
+     </div>
+
+     <div className="form-control w-full ">
+          <label className="label">
+         <p className="label-text mb-1 text-[19px] font-medium">Price<span className='text-red-800'>*</span></p>
+        </label>
+        <input type="number" {...register("price", { required: true })} placeholder="price" className="input input-bordered w-full text-lg" />
+     </div>
+
+
+  </div>
+
+  <div className="form-control mb-6">
+   <label className="label">
+   <p className="label-text mb-1 text-[19px] font-medium">Recipe details<span className='text-red-800'>*</span></p>
+   </label>
+   <textarea {...register("details", { required: true })} className="textarea textarea-bordered h-24 resize-none  text-lg" placeholder="recepie details"></textarea>
+ </div>
+  
+   <div className="form-control max-w-sm mb-4">
+     <input type="file" {...register("image", { required: true })}  className="file-input file-input-bordered file-input-sm w-full " />
+   </div>
+        <button className='flex items-center text-[21px] font-medium text-white px-3 py-[9px] rounded bg-[#D1A454]'>Add item <FaUtensils className='mx-2'></FaUtensils></button>
+          
+           </form>
         </div>
     );
 };
